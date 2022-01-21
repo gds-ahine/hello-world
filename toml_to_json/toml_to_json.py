792069f0b934883449e9327093da8e64d1c913f9
Aaron Hine <aaron.hine@digital.cabinet-office.gov.uk>
2022-01-21 11:24:23 +0000

    testing toml to json

--- toml_to_json/toml_to_json.py
+++ toml_to_json/toml_to_json.py
@@ -5,3 +5,8 @@ toml_file = toml.load('accounts_loggroup_index.toml')
 
 with open('accounts_loggroup_index.json', 'w') as json_file:
 	json_file.write(json.dumps(toml_file))
+
+with open('accounts_loggroup_index.json', 'r') as f:
+	content = f.read()
+	print(content)
+	f.close()
