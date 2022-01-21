f90127552027d157540b5b7f7ca6b7aa37be955c
Aaron Hine <aaron.hine@digital.cabinet-office.gov.uk>
2022-01-21 12:44:36 +0000

    testing toml to json

--- kinesis_processor/convert_cloudwatch_routing_toml_to_json.py
+++ kinesis_processor/convert_cloudwatch_routing_toml_to_json.py
@@ -1,3 +1,5 @@
+# Convert Cloudwatch TOML routing file to JSON
+
 import toml
 import json
 
@@ -5,8 +7,3 @@ toml_file = toml.load('accounts_loggroup_index.toml')
 
 with open('accounts_loggroup_index.json', 'w') as json_file:
 	json_file.write(json.dumps(toml_file))
-
-with open('accounts_loggroup_index.json', 'r') as f:
-	content = f.read()
-	print(content)
-	f.close()
